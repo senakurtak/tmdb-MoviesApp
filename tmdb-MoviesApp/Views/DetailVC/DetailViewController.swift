8207d02f521831057650a1c61b79c90e765d7094
senakurtak <zskurtak@gmail.com>
2022-12-28 04:48:11 +0300

    saving issues from detail page

--- tmdb-MoviesApp/Views/DetailVC/DetailViewController.swift
+++ tmdb-MoviesApp/Views/DetailVC/DetailViewController.swift
@@ -21,24 +21,32 @@ class DetailViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
+    
+    @IBOutlet weak var buttonFav: UIButton!
+    
     var favoriteCheck : Bool = true
     
     var selectedMovie : Movie?
-        
+    
     var savedMovieList : [Movie] = []
     
     let realm = try! Realm()
     
-    var isMovieSaved : Bool = false
-    var indexOfMovie : Int = 0
+    //    var isMovieSaved : Bool = false
+    //    var indexOfMovie : Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUpMovie()
-//        retrieveMovieDetails()
-//        configureFavouriteButton()
+        configure()
+        //        retrieveMovieDetails()
+        //        configureFavouriteButton()
     }
     
+    
+    func configure(){
+        checkFav()
+        setUpMovie()
+    }
     func setUpMovie(){
         if let movies = selectedMovie{
             titleLabel.text = movies.title
@@ -52,65 +60,97 @@ class DetailViewController: UIViewController {
     }
     
     
-    @IBAction func didTappedFavouriteButton(_ sender: UIButton) {
-        let image = favoriteCheck ? UIImage(systemName: "heart.fill") : UIImage(systemName:"heart")
-        sender.setImage(image, for: .normal)
-        print("Fav button tapped for \(selectedMovie!.title)")
-//        renderWithRealm()
-        favoriteCheck.toggle()
-        
-//        if (!isMovieSaved){
-//            let savedMovie = FavoriteMovies(isSaved: true,
-//                                            backdropPath: selectedMovie?.backdropPath ?? "",
-//                                            overview: selectedMovie?.overview ?? "",
-//                                            popularity: selectedMovie?.popularity ?? 0.0,
-//                                            posterPath: selectedMovie?.posterPath ?? "",
-//                                            releaseDate: selectedMovie?.releaseDate ?? "",
-//                                            title: selectedMovie?.title ?? "",
-//                                            voteAverage: selectedMovie?.voteAverage ?? 0.0)
-//            self.savedMovieList.append(savedMovie)
-//            UserDefaults.standard.set(try? PropertyListEncoder().encode(self.savedMovieList), forKey:"favoriteMovies")
-//
-//        } else {
-//            self.savedMovieList.remove(at: indexOfMovie)
-//            UserDefaults.standard.set(try? PropertyListEncoder().encode(self.savedMovieList), forKey: "favoriteMovies")
-//        }
-//        print("savedMovieList dizisinin \(savedMovieList.count) tane elemanı vardır  \(savedMovieList[0])")
-    
-    }
-    
-//    func retrieveMovieDetails() {
-//
-//        if let data = UserDefaults.standard.value(forKey: "favoriteMovies") as? Data {
-//            let favoriteMovies = try? PropertyListDecoder().decode(Array<FavoriteMovies>.self, from: data)
-//            savedMovieList = favoriteMovies ?? []
-//        }
-//
-//    }
-//    func configureFavouriteButton(){
-//                        if (isMovieSaved) {
-//                            favouriteButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
-//                        } else {
-//                            favouriteButton.setImage(UIImage(systemName: "heart"), for: .normal)
-//                        }
-//    }
-    
-    private func checkIfIsSaved(movieName: String?) -> Bool {
-        if !savedMovieList.contains(where: { $0.title == movieName }) {
-            return false
-        } else {
-            return true
-        }
-    }
+    func checkFav(){
+        if CoreDataHandler.shared.savedArr.contains(where: {$0.id == selectedMovie!.id}){
+            CoreDataHandler.shared.deleteData(movie: selectedMovie!)
+            buttonFav.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+        }else{
+            buttonFav.setImage(UIImage(systemName: "heart.fill"), for: .normal)
 
-    func checkIfMovieWillSaved(movieName : String?) -> Bool {
-        if savedMovieList.contains(where: {$0.title == movieName}){
-            return false
-        } else {
-            return true
         }
     }
     
+    
+    @IBAction func didTappedFavouriteButton(_ sender: UIButton) {
+        
+        if CoreDataHandler.shared.savedArr.contains(where: {$0.id == selectedMovie!.id}){
+            CoreDataHandler.shared.deleteData(movie: selectedMovie!)
+            buttonFav.setImage(UIImage(systemName: "heart"), for: .normal)
+            
+        } else {
+            CoreDataHandler.shared.saveToCoreData(movie: selectedMovie!)
+            buttonFav.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+            
+        }
+        
+        //        let image = favoriteCheck ? UIImage(systemName: "heart.fill") : UIImage(systemName:"heart")
+        //        sender.setImage(image, for: .normal)
+        //        print("Fav button tapped for \(selectedMovie!.title)")
+        //        //        renderWithRealm()
+        //        favoriteCheck.toggle()
+        
+        //        if (!isMovieSaved){
+        //            let savedMovie = FavoriteMovies(isSaved: true,
+        //                                            backdropPath: selectedMovie?.backdropPath ?? "",
+        //                                            overview: selectedMovie?.overview ?? "",
+        //                                            popularity: selectedMovie?.popularity ?? 0.0,
+        //                                            posterPath: selectedMovie?.posterPath ?? "",
+        //                                            releaseDate: selectedMovie?.releaseDate ?? "",
+        //                                            title: selectedMovie?.title ?? "",
+        //                                            voteAverage: selectedMovie?.voteAverage ?? 0.0)
+        //            self.savedMovieList.append(savedMovie)
+        //            UserDefaults.standard.set(try? PropertyListEncoder().encode(self.savedMovieList), forKey:"favoriteMovies")
+        //
+        //        } else {
+        //            self.savedMovieList.remove(at: indexOfMovie)
+        //            UserDefaults.standard.set(try? PropertyListEncoder().encode(self.savedMovieList), forKey: "favoriteMovies")
+        //        }
+        //        print("savedMovieList dizisinin \(savedMovieList.count) tane elemanı vardır  \(savedMovieList[0])")
+        
+    }
+    
+    
+    //    func retrieveMovieDetails() {
+    //
+    //        if let data = UserDefaults.standard.value(forKey: "favoriteMovies") as? Data {
+    //            let favoriteMovies = try? PropertyListDecoder().decode(Array<FavoriteMovies>.self, from: data)
+    //            savedMovieList = favoriteMovies ?? []
+    //        }
+    //
+    //    }
+    //    func configureFavouriteButton(){
+    //                        if (isMovieSaved) {
+    //                            favouriteButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+    //                        } else {
+    //                            favouriteButton.setImage(UIImage(systemName: "heart"), for: .normal)
+    //                        }
+    //    }
+    
+    
+    
+    
+    
+    
+    
+    //    private func checkIfIsSaved(movieName: String?) -> Bool {
+    //        if !savedMovieList.contains(where: { $0.title == movieName }) {
+    //            return false
+    //        } else {
+    //            return true
+    //        }
+    //    }
+    //
+    //    func checkIfMovieWillSaved(movieName : String?) -> Bool {
+    //        if savedMovieList.contains(where: {$0.title == movieName}){
+    //            return false
+    //        } else {
+    //            return true
+    //        }
+    //    }
+    //
+    
+    
+    // Realm ile işlemler için
     //    func save(){
     //        let favSelectedMovie = FavoriteMovie()
     //        favSelectedMovie.movieTitle = selectedMovie!.title
@@ -136,6 +176,20 @@ class DetailViewController: UIViewController {
     //        render()
     //
     //    }
+    
+    //    @IBAction func favButtonClicked(_ sender: Any) {
+    //
+    //        if CoreDataHandler.shared.savedArr.contains(where: {$0.id == selectedMovie!.id}){
+    //            CoreDataHandler.shared.deleteData(movie: selectedMovie!)
+    //            favBtn.setImage(UIImage(systemName: "heart"), for: .normal)
+    //
+    //        } else {
+    //            CoreDataHandler.shared.saveToCoreData(movie: selectedMovie!)
+    //            favBtn.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+    //
+    //        }
+    //    }
+    
 }
 
 
